d4987f51edc88391c27bc8753a995fcbfb21f452
roman-shneer <romanshneer@gmail.com>
2026-07-08 15:06:58 +0300

    added tracking_id to response

--- services/ingestion/tests/app.test.js
+++ services/ingestion/tests/app.test.js
@@ -79,7 +79,7 @@ describe('Ingestion Service API Tests', () => {
         expect(response.body).toEqual({
             tracking_id: "INV-1016",
             status: "ACCEPTED",
-            message: "Invoice submitted successfully and queued for processing."
+            message: "Invoice submitted successfully and queued for processing.[INV-1016]"
         });
 
         expect(mockStateGet).toHaveBeenCalledWith('approval-state', expect.any(String));
